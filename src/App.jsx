a3af2e6432109023224dc40a406fd37bc2491272
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-03-30 23:22:07 +0200

    Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './assets/sass/main.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Nyxy Portfolio</h1>
+      <div className="card">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
