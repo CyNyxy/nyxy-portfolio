626a793e5fc0a6f5936de3abccdaaae3a4fd85dd
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-04-07 05:12:16 +0200

    Fixed img paths and path config

--- src/containers/Filter/Filter.jsx
+++ src/containers/Filter/Filter.jsx
@@ -12,7 +12,7 @@ function Filter () {
       id: 1, 
       tags: '#HTML #CSS',
       name: 'Gallery Page', 
-      img: '../../src/assets/img/t_dev01.png',
+      img: 'src/assets/img/t_dev01.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
       cats: 'html' 
     },
@@ -20,7 +20,7 @@ function Filter () {
       id: 2, 
       tags: '#HTML #CSS',
       name: '404 not found Page', 
-      img: '../../src/assets/img/t_dev02.png',
+      img: 'src/assets/img/t_dev02.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
       cats: 'css'
     },
@@ -28,7 +28,7 @@ function Filter () {
       id: 3, 
       tags: '#HTML #CSS #React',
       name: 'Interior Consultant', 
-      img: '../../src/assets/img/t_dev03.png',
+      img: 'src/assets/img/t_dev03.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
       cats: 'react'
     },
@@ -36,7 +36,7 @@ function Filter () {
       id: 4, 
       tags: '#HTML #CSS',
       name: 'Team Page', 
-      img: '../../src/assets/img/t_dev04.png',
+      img: 'src/assets/img/t_dev04.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
       cats: 'react'
     },
@@ -44,7 +44,7 @@ function Filter () {
       id: 5, 
       tags: '#HTML #CSS',
       name: 'Gallery Page', 
-      img: '../../src/assets/img/t_dev05.png',
+      img: 'src/assets/img/t_dev05.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
       cats: 'react'
     },
@@ -52,7 +52,7 @@ function Filter () {
       id: 6, 
       tags: '#HTML #CSS #Javascript',
       name: 'Checkout Page', 
-      img: '../../src/assets/img/t_dev06.png',
+      img: 'src/assets/img/t_dev06.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
       cats: 'react'
     },
@@ -60,7 +60,7 @@ function Filter () {
       id: 7, 
       tags: '#HTML #CSS #React',
       name: 'Edie Homepage', 
-      img: '../../src/assets/img/t_dev07.png',
+      img: 'src/assets/img/t_dev07.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
       cats: 'react'
     },
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sass from 'sass';
+import sass from 'sass'
 import autoprefixer from 'autoprefixer'
 import sortMediaQueries from 'postcss-sort-media-queries'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react()],
+    resolve: {
+      alias: {
+        src: path.resolve('src/'),
+      },
+    },
     build: {
       css: {
         preprocessorOptions: {
