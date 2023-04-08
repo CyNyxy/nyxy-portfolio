e177eae21e3fecfe5cd61ed2680f396aaa3827e7
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-04-08 02:51:58 +0200

    Filter added with multiple active options

--- src/containers/Projects/Projects.jsx
+++ src/containers/Projects/Projects.jsx
@@ -1,20 +1,28 @@
 import { useState } from 'react';
 
 function Projects () {
-  const [activeCategories, setActiveCategories] = useState('all');
+  const [activeCategories, setActiveCategories] = useState([]);
+
+  const allButtonClass = activeCategories.length === 0 ? 'active' : undefined;
 
   const filterItem = (category) => {
-    setActiveCategories(category);
+    setActiveCategories(prevCategories => {
+      if (prevCategories.includes(category)) {
+        return prevCategories.filter((c) => c !== category);
+      } else {
+        return [...prevCategories, category];
+      }
+    });
   };
 
   const listItems = [
     { 
       id: 1, 
       tags: '#HTML #CSS',
-      name: 'Gallery Page', 
+      name: 'Recipe Page', 
       img: 'assets/t_dev01.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
-      cats: 'html' 
+      categories: ['html', 'css'] 
     },
     { 
       id: 2, 
@@ -22,7 +30,7 @@ function Projects () {
       name: '404 not found Page', 
       img: 'assets/t_dev02.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
-      cats: 'css'
+      categories: ['css']
     },
     { 
       id: 3, 
@@ -30,58 +38,29 @@ function Projects () {
       name: 'Interior Consultant', 
       img: 'assets/t_dev03.png',
       desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
-      cats: 'react'
-    },
-    { 
-      id: 4, 
-      tags: '#HTML #CSS',
-      name: 'Team Page', 
-      img: 'assets/t_dev04.png',
-      desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
-      cats: 'css'
-    },
-    { 
-      id: 5, 
-      tags: '#HTML #CSS',
-      name: 'Gallery Page', 
-      img: 'assets/t_dev05.png',
-      desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
-      cats: 'css'
-    },
-    { 
-      id: 6, 
-      tags: '#HTML #CSS #Javascript',
-      name: 'Checkout Page', 
-      img: 'assets/t_dev06.png',
-      desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
-      cats: 'html'
-    },
-    { 
-      id: 7, 
-      tags: '#HTML #CSS #React',
-      name: 'Edie Homepage', 
-      img: 'assets/t_dev07.png',
-      desc: 'In this project, I work with HTML and CSS to create a responsive page . The design is from devchallenge.io. Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie.',
-      cats: 'react'
+      categories: ['react']
     },
   ];
 
+
   const filteredListItems =
-    activeCategories === 'all'
+    activeCategories.length === 0
       ? listItems
-      : listItems.filter((item) => item.cats === activeCategories);
+      : listItems.filter((item) =>
+          item.categories.some((c) => activeCategories.includes(c))
+        );
 
   return (
     <div>
       <div className="filter-box">
-        <button onClick={() => filterItem('all')}>All</button>
-        <button onClick={() => filterItem('html')}>HTML</button>
-        <button onClick={() => filterItem('css')}>CSS</button>
-        <button onClick={() => filterItem('react')}>React</button>
+      <button className={allButtonClass} onClick={() => setActiveCategories([])}>All</button>
+        <button className={activeCategories.includes('html') ? 'active' : ''} onClick={() => filterItem('html')}>HTML</button>
+        <button className={activeCategories.includes('css') ? 'active' : ''} onClick={() => filterItem('css')}>CSS</button>
+        <button className={activeCategories.includes('react') ? 'active' : ''} onClick={() => filterItem('react')}>React</button>
       </div>
       <ul className="project-list">
         {filteredListItems.map((item) => (
-          <li key={item.id} className="js-filterable" data-category={item.cats}>
+          <li key={item.id} className="js-filterable" data-category={item.categories.join(' ')}>
             <div className="box flex">
               <div>
                 <img className="thumb" src={item.img} alt="gallerypage" />
